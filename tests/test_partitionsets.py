c70a6189c66308138940233a2b7a626743ad8620
Stefan Hagen (sthagen) <sthagen@cisco.com>
2016-10-03 20:58:22 +0200

    Further tests for cli options and removal of a pytest warning (section name in setup.cfg)

--- tests/test_partitionsets.py
+++ tests/test_partitionsets.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 
 from partitionsets.cli import main
 
@@ -52,3 +54,47 @@ def test_main_bell_numbers():
         raise UserWarning("This should not have returned!")
     except SystemExit:
         pass
+
+
+def test_main_q_json():
+    arg_tv = [
+        '-q',
+        '-T', 'json',
+        '3 42 -1'
+    ]
+    assert main(arg_tv) == 0
+
+
+def test_main_q_csv():
+    arg_tv = [
+        '-q',
+        '-T', 'csv',
+        '3 42 -1'
+    ]
+    assert main(arg_tv) == 0
+
+
+def test_main_q_o():
+    tmp_dir = tempfile.mkdtemp()
+    predictable_filename = 'myfile'
+    # Ensure the file is read/write by the creator only
+    save_umask = os.umask(0o0077)
+
+    o_path = os.path.join(tmp_dir, predictable_filename)
+    try:
+        arg_tv = [
+            '-q',
+            '-o', o_path,
+            '3 42 -1'
+        ]
+        assert main(arg_tv) == 0
+    except IOError as e:
+        print 'Unexpected IOError, maybe build/test system exoctic?'
+    else:
+        try:
+            os.remove(o_path)
+        except OSError:
+            pass  # file already removed ...
+    finally:
+        os.umask(save_umask)
+        os.rmdir(tmp_dir)
